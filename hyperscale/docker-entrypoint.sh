1fe2a6c149a397c697e362f24f55b7389c04b35c
gindibay <gurkanindibay@gmail.com>
2021-11-21 14:13:34 +0300

    Fixes review comments

--- hyperscale/docker-entrypoint.sh
+++ hyperscale/docker-entrypoint.sh
@@ -332,7 +332,9 @@ _main() {
 
 			docker_temp_server_stop
 			# entry point unsets PGPASSWORD, but we need it to connect to workers
+			# https://github.com/docker-library/postgres/blob/33bccfcaddd0679f55ee1028c012d26cd196537d/12/docker-entrypoint.sh#L303
 # 			unset PGPASSWORD
+            cp /pg_hba.conf ${PGDATA}/pg_hba.conf
 
 			echo
 			echo 'PostgreSQL init process complete; ready for start up.'
